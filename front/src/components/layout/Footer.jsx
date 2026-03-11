8617b8ea4a9febf2ed55a3a34861dc28e5387e5c
yohan frutiger <yohan.frutiger@gmail.com>
2026-03-11 10:58:42 +0100

    Modification du footer

--- front/src/components/layout/Footer.jsx
+++ front/src/components/layout/Footer.jsx
@@ -3,12 +3,12 @@ import logo from "../../assets/logo.png";
 
 export default function Footer() {
   return (
-    <footer className="h-[120px] bg-green/90 text-white mt-16 ">
-      <div className="h-full max-w-7xl mx-auto flex items-center justify-between relative ">
+    <footer className="h-[80px] bg-green/90 text-white mt-16 ">
+      <div className="h-full max-w-7xl mx-auto flex items-center justify-between ">
         {/* COLONNE GAUCHE */}
-        <div className="flex flex-col gap-1 lg:gap-3 ml-2 md:ml-5 ">
+        <div className="flex flex-col  ml-2 md:ml-5">
           <p className="text-base">
-            Canopées © 2025
+            SARL Canopées
           </p>
           <p className="text-base">
             25 rue Rossignol 07320 Saint-Agrève
@@ -18,14 +18,14 @@ export default function Footer() {
           </p>
         </div>
         {/* COLONNE DROITE  */}
-        <div className="flex flex-col items-end text-right gap-1 lg:gap-3 mr-2 md:mr-5 ">
+        <div className="flex flex-col items-end text-right mr-2 md:mr-5 ">
           <a
             href="mailto:contact@canopees.fr"
             className="hover:text-orange transition "
           >
             contact@canopees.fr
           </a>
-          <div className="flex flex-col gap-1 lg:gap-3">
+          <div className="flex flex-col">
             <NavLink to="/legal-notices" className="hover:text-orange transition">
               Mentions légales
             </NavLink>
@@ -36,11 +36,14 @@ export default function Footer() {
         </div>
         {/* LOGO */}
         <div className="absolute left-1/2 -translate-x-1/2 hidden md:block">
-          <img
+        <p className="text-xl">
+            Canopées © 2025
+          </p>
+          {/* <img
             src={logo}
             alt="Canopées"
             className="p-5 h-[65px] lg:h-[70px] w-auto object-contain"
-          />
+          /> */}
         </div>
       </div>
     </footer>
