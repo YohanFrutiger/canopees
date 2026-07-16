166301db3fd5239808393ad85d69c60b098be9bc
yohan frutiger <yohan.frutiger@gmail.com>
2026-07-16 23:01:24 +0200

    style : modification de la taille de la police et du padding

--- front/src/components/features/ContactButton.jsx
+++ front/src/components/features/ContactButton.jsx
@@ -6,8 +6,8 @@ import { Link } from 'react-router-dom'
 // Composant ContactButton
 export default function ContactButton({ btnContent }) {
   return (
-    <div className="mt-16 mb-8 text-center max-w-72 mx-auto ">
-      <Link to="/contact" className=" text-xl px-4 text-center py-3 bg-blue hover:bg-blue/80 text-white rounded-lg font-semibold shadow-lg transition transform hover:scale-105">
+    <div className="mt-16 mb-8 text-center w-72 ">
+      <Link to="/contact" className=" sm:text-xl p-2 sm:p-4 text-center  bg-blue hover:bg-blue/80 text-white rounded-lg font-semibold shadow-lg transition transform hover:scale-105">
         {btnContent}
       </Link>
     </div>
