9129576934c7952ad17441bb16a990c5fa611d50
yohan frutiger <yohan.frutiger@gmail.com>
2026-07-20 15:08:50 +0200

    Ajout d'un breakpoint pour la taille des polices

--- front/src/components/layout/Footer.jsx
+++ front/src/components/layout/Footer.jsx
@@ -3,22 +3,22 @@ import logo from "../../assets/logo.png";
 
 export default function Footer() {
   return (
-    <footer className="h-[80px] bg-green/90 text-white mt-16 ">
+    <footer className="h-[80px] bg-green/90 text-white mt-16 min-w-80">
       <div className="h-full max-w-7xl mx-auto flex items-center justify-between ">
         {/* COLONNE GAUCHE */}
-        <div className="flex flex-col ml-2 md:ml-5 font-thin text-sm gap-1">
-          <p className="text-sm">
+        <div className="flex flex-col ml-2 md:ml-5 font-thin gap-1">
+          <p className="text-xs sm:text-sm">
             SARL Canopées
           </p>
-          <p className="text-sm">
+          <p className="text-xs sm:text-sm">
             25 rue Rossignol 07320 Saint-Agrève
           </p>
-          <p className="text-sm">
+          <p className="text-xs sm:text-sm">
             04 72 32 45 67
           </p>
         </div>
         {/* COLONNE DROITE  */}
-        <div className="flex flex-col items-end text-right mr-2 md:mr-5 font-thin gap-1 text-sm">
+        <div className="flex flex-col items-end text-right mr-2 md:mr-5 font-thin gap-1 text-xs sm:text-sm">
           <a
             href="mailto:contact@canopees.fr"
             className="hover:text-orange transition "
